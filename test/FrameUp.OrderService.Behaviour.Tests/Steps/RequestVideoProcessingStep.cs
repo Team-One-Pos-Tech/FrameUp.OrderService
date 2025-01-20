ec29a3d77f95ff2abd66bb33e281056039c68cbc
Kássio San <kciosantana@gmail.com>
2025-01-20 16:03:55 -0300

    #1 Request a video to be processed

--- test/FrameUp.OrderService.Behaviour.Tests/Steps/RequestVideoProcessingStep.cs
+++ test/FrameUp.OrderService.Behaviour.Tests/Steps/RequestVideoProcessingStep.cs
@@ -1,6 +1,8 @@
+using FluentAssertions;
 using FrameUp.OrderService.Api.Models;
 using FrameUp.OrderService.Behaviour.Tests.Fixtures;
 using FrameUp.OrderService.Behaviour.Tests.Helpers;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -10,7 +12,10 @@ using TechTalk.SpecFlow;
 namespace FrameUp.OrderService.Behaviour.Tests.Steps;
 
 [Binding]
-public class RequestVideoProcessingStep(OrderServiceClientApi orderServiceClientApi): TestHelpers
+public class RequestVideoProcessingStep(
+    OrderServiceClientApi orderServiceClientApi,
+    ScenarioContext scenarioContext
+    ) : TestHelpers
 {
     [BeforeScenario]
     public async Task Setup()
@@ -18,33 +23,43 @@ public class RequestVideoProcessingStep(OrderServiceClientApi orderServiceClient
     }
     
     [Given(@"I have a video file")]
-    public async Task GivenIHaveAVideoFileAsync()
+    public void GivenIHaveAVideoFileAsync()
     {
-        var resolution = 1080;
-
-        var files = new List<FileParameter>
+        var videos = new List<FileParameter>
         {
             new(CreateFakeVideo(), "video.mp4", "video/mp4")
         };
 
-        await orderServiceClientApi.OrderAsync(resolution, files);
+        scenarioContext["videos"] = videos;
     }
 
     [Given(@"Define Parameters")]
     public void GivenDefineParameters()
     {
-        ScenarioContext.StepIsPending();
+        var resolution = 1080;
+
+        scenarioContext["resolution"] = resolution;
     }
 
     [When(@"I request the video to be processed")]
-    public void WhenIRequestTheVideoToBeProcessed()
+    public async Task WhenIRequestTheVideoToBeProcessedAsync()
     {
-        ScenarioContext.StepIsPending();
+        var resolution = scenarioContext.Get<int>("resolution");
+        var videos = scenarioContext.Get<List<FileParameter>>("videos");
+
+        var response = await orderServiceClientApi.OrderAsync(resolution, videos);
+
+        scenarioContext["response"] = response;
     }
 
     [Then(@"the video should start processing")]
     public void ThenTheVideoShouldStartProcessing()
     {
-        ScenarioContext.StepIsPending();
+        var response = scenarioContext.Get<CreateProcessingOrderResponse>("response");
+
+        response.IsValid.Should().BeTrue();
+
+        response.Status.Should().Be(ProcessingStatus.Processing);
+
     }
 }
