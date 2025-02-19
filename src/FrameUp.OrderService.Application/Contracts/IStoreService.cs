1429485f594de138a58e2dbd75038d4d206990d1
Kássio San <kciosantana@gmail.com>
2025-02-18 21:09:38 -0300

    draft service

---
+++ src/FrameUp.OrderService.Application/Contracts/IStoreService.cs
@@ -0,0 +1,24 @@
+﻿namespace FrameUp.OrderService.Application.Contracts;
+
+public interface IStoreService
+{
+    public Task Save(Guid orderId, Stream[] files);
+}
+
+public class StoreService : IStoreService
+{
+    public async Task Save(Guid orderId, Stream[] files)
+    {
+        string directoryPath = Path.Combine("Orders", orderId.ToString());
+        Directory.CreateDirectory(directoryPath);
+
+        for (int i = 0; i < files.Length; i++)
+        {
+            string filePath = Path.Combine(directoryPath, $"file_{i}.dat");
+            using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                await files[i].CopyToAsync(fileStream);
+            }
+        }
+    }
+}
