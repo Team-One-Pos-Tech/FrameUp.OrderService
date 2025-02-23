af7f87de00526cb6a40a93ca209d7194af5ce579
Kássio San <kciosantana@gmail.com>
2025-02-23 12:27:47 -0300

    impelment local store repository

---
+++ src/FrameUp.OrderService.Infra/Repositories/LocalStoreRepository.cs
@@ -0,0 +1,40 @@
+namespace FrameUp.OrderService.Infra.Repositories;
+
+public class LocalStoreRepository : ILocalStoreRepository
+{
+    private const string BasePath = "C:\\FrameUp\\LocalStore";
+
+    public async Task SaveFileAsync(Guid orderId, string fileName, Stream contentStream)
+    {
+        var path = Path.Combine(BasePath, orderId.ToString(), fileName);
+
+        Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+        using (var fileStream = File.Create(path))
+        {
+            await contentStream.CopyToAsync(fileStream);
+        }
+    }
+
+    public async Task<Stream> GetFileAsync(Guid orderId, string fileName)
+    {
+        var path = Path.Combine(BasePath, orderId.ToString(), fileName);
+
+        if (!File.Exists(path))
+            throw new FileNotFoundException("File not found", path);
+
+        return File.OpenRead(path);
+    }
+
+    public Task DeleteFileAsync(Guid orderId, string fileName)
+    {
+        var path = Path.Combine(BasePath, orderId.ToString(), fileName);
+
+        if (!File.Exists(path))
+            throw new FileNotFoundException("File not found", path);
+
+        File.Delete(path);
+
+        return Task.CompletedTask;
+    }
+}
